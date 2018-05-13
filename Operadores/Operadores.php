9af5ef4db8b47bd1280774de342dfb4c96c49279
G. Verde <gabrielhsverde@outlook.com>
2018-05-13 19:04:36 -0300

    Exemplos de Operadores

--- Operadores/Operadores.php
+++ Operadores/Operadores.php
@@ -9,7 +9,6 @@ $nomeCompleto .=$segundoNome;
 echo $nomeCompleto . "<br><br>";
 
 $valorTotal = 0;
-
 $valorTotal += 100;
 $valorTotal += 30;
 $valorTotal -= 10;
@@ -28,4 +27,32 @@ echo $a / $b . "<br>";
 echo $a % $b . "<br>";
 echo $a ** $b . "<br>";
 
+//Operadores de Comparação
+echo "<h3>Operadores de Comparação</h3>";
+$a = 30;
+$b = 55;
+
+var_dump($a > $b);
+echo "<br>";
+var_dump($a < $b);
+echo "<br>";
+var_dump($a == $b);
+echo "<br>";
+var_dump($a != $b);
+
+//Operadores Especiais do PHP 7
+echo "<h3>Operadores Especiais do PHP7</h3>";
+$a = 55;
+$b = 35;
+
+//Spaceship
+var_dump($a <=> $b);
+
+//Null Coalesce
+echo "<h3>Null Coalesce</h3>";
+$null1 = null;
+$null = null;
+$valor = 10;
+
+echo $null1 ?? $null ?? "Valor Padrão";
 ?>
---
+++ Operadores/Operadores2.php
@@ -0,0 +1,18 @@
+<?php
+$a = 10;
+
+echo $a++ . "<br>";
+echo $a . "<br><br>";
+
+$b = 10;
+echo ++$b. "<br>";
+echo $b . "<br><br>";
+
+$c = 10;
+echo $c-- . "<br>";
+echo $c . "<br><br>";
+
+$d = 10;
+echo --$c . "<br>";
+echo $c;
+?>
