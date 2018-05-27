e8d34b07fbcc9bc40c0d80ad626cd261dc3d7e66
G. Verde <gabrielhsverde@outlook.com>
2018-05-27 17:37:52 -0300

    Constantes/Arrays Constantes

---
+++ Arrays/Constante.php
@@ -0,0 +1,17 @@
+<?php
+define('PI', 3.1416, true);
+
+echo PI."<br><br>";
+
+//Arrays Constantes
+define("BANCO_DE_DADOS", [
+    '127.0.0.1',
+    'root',
+    'password',
+    'test'
+]);
+
+print_r(BANCO_DE_DADOS);
+
+echo DIRECTORY_SEPARATOR;
+?>
