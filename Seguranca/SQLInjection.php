b7cf390d68a0a9a6d17a93e110c9a0594379e7fb
Gabriel Verde <gabrielhsverde@outlook.com>
2018-07-23 16:44:07 -0300

    SQL Injection

--- Seguranca/SQLInjection.php
+++ Seguranca/SQLInjection.php
@@ -2,8 +2,14 @@
 
 $id = (isset($_GET["id"])) ? $_GET['id'] : 1;
 
-$con = mysqli("localhost", "root", "", "dbphp7");
+$con = mysqli_connect("localhost", "root", "", "dbphp7");
 
 $sql = "select * from tb_usuarios where idusuario = $id";
+
+$exec = mysqli_query($con, $sql);
+
+while ($resultado = mysqli_fetch_object($exec)) {
+    echo $resultado->deslogin."<br>";
+}
 
 ?>
